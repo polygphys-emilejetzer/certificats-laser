aabb8a5b9940184aad4ebaad0c8050d4784cbb96
Émile Jetzer <emile.jetzer@polymtl.ca>
2022-06-02 10:24:43 -0400

    Spécification des modules requis

--- nouveau_certificats.py
+++ nouveau_certificats.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
-"""Créer de nouveaux certificats de sureté laser."""
+"""Créer de nouveaux certificats laser au besoin."""
 
-# Bibliothèque standard
-import getpass
+# Bibliothèques standard
+import subprocess
+import time
 
-from pathlib import Path
-from datetime import datetime as dt
-from subprocess import run
+from pathlib import Pat
 
 # Bibliothèque PIPy
-import pptx
-import keyring
+import schedule
 
-# Imports relatifs
-from ...outils.reseau.msforms import MSFormConfig, MSForm
-from ...outils.reseau import DisqueRéseau
+# Bibliothèques maison
+from polygphys.outils.reseau.msforms import MSFormConfig, MSForm
+from polygphys.outils.reseau import DisqueRéseau
 
+# Définitions de classes
 
 class SSTLaserCertificatsConfig(MSFormConfig):
 
@@ -84,3 +83,29 @@ class SSTLaserCertificatsForm(MSForm):
                          '-o',
                          str(fichier_pdf),
                          str(fichier)])
+
+# Programme
+
+chemin_config = Path('~').expanduser() / 'certificats_laser.cfg'
+config = SSTLaserCertificatsConfig(chemin_config)
+
+dossier = OneDrive('',
+                   config.get('onedrive', 'organisation'),
+                   config.get('onedrive', 'sous-dossier'),
+                   partagé=True)
+fichier = dossier / config.get('formulaire', 'nom')
+config.set('formulaire', 'chemin', str(fichier))
+
+formulaire = SSTLaserCertificatsForm(config)
+
+exporteur = subprocess.Popen(['unoconv', '--listener'])
+
+schedule.every().day.at('08:00').do(formulaire.mise_à_jour)
+
+formulaire.mise_à_jour()
+try:
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+finally:
+    exporteur.terminate()
